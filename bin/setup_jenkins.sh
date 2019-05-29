7808ac2c17eff106715d80598cca1a283cf899e2
Kevin Dubois <kevin.dubois@gmail.com>
2019-05-29 14:03:56 +0200

    move oc resources a little lower

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,10 +15,6 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
 
-oc set resources dc/jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1 -n ${GUID}-jenkins
-
-oc set volume dc/jenkins --add --overwrite --name=jenkins-data --mount-path=/var/lib/jenkins --type persistentVolumeClaim --claim-name=${GUID}-jenkins-pvc --claim-size=4Gi -n ${GUID}-jenkins
-
 # Create custom agent container image with skopeo
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
       USER root\nRUN yum -y install skopeo && yum clean all\n
@@ -43,6 +39,10 @@ items:
           jenkinsfilePath: Jenkinsfile
 kind: List
 metadata: []" | oc create -f - -n ${GUID}-jenkins
+
+oc set resources dc/jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1 -n ${GUID}-jenkins
+
+oc set volume dc/jenkins --add --overwrite --name=jenkins-data --mount-path=/var/lib/jenkins --type persistentVolumeClaim --claim-name=${GUID}-jenkins-pvc --claim-size=4Gi -n ${GUID}-jenkins
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
